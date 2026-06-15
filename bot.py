0fd361bd63eb1c70a985f466f6b67c610e4a0e46
carzyben94 <crazy.ben.laden94@gmail.com>
2026-06-15 09:51:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import os
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
+from aiogram.types import Message
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
-async def start_cmd(message: types.Message):
+async def start_cmd(message: Message):
     await message.answer("Бот работает")
 
 async def main():
