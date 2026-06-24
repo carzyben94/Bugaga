a9fcb0908c8b0d3dd228939065bf6daf4f3c9fa3
carzyben94 <crazy.ben.laden94@gmail.com>
2026-06-24 21:31:07 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -6,42 +6,73 @@ class CloakBrowserManager:
         self.page = None
 
     async def start(self, proxy=None):
-        """Запуск браузера через launch_async"""
+        """Запуск браузера с улучшенной эмуляцией"""
         self.browser = await launch_async(
             headless=True,
             proxy=proxy,
             humanize=True,
-            args=['--no-sandbox', '--disable-dev-shm-usage']
+            args=[
+                '--no-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-blink-features=AutomationControlled',
+                '--disable-features=IsolateOrigins,site-per-process',
+                '--disable-web-security',
+                '--disable-features=BlockInsecurePrivateNetworkRequests',
+                '--disable-features=OutOfBlinkCors',
+                '--disable-gpu',
+                '--disable-setuid-sandbox',
+                '--disable-infobars',
+                '--disable-breakpad',
+                '--disable-component-extensions-with-background-pages',
+                '--disable-default-apps',
+                '--disable-extensions',
+                '--disable-features=TranslateUI',
+                '--disable-ipc-flooding-protection',
+                '--disable-renderer-backgrounding',
+                '--disable-backgrounding-occluded-windows',
+                '--disable-background-timer-throttling',
+                '--disable-client-side-phishing-detection',
+                '--disable-crash-reporter',
+                '--disable-hang-monitor',
+                '--disable-prompt-on-repost',
+                '--disable-sync',
+                '--no-first-run'
+            ]
         )
-        self.page = await self.browser.new_page()
+        
+        self.page = await self.browser.new_page(
+            viewport={'width': 1280, 'height': 720},
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        )
+        
+        # Скрываем следы автоматизации
+        await self.page.add_init_script("""
+            Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+            });
+            Object.defineProperty(navigator, 'plugins', {
+                get: () => [1, 2, 3, 4, 5]
+            });
+            Object.defineProperty(navigator, 'languages', {
+                get: () => ['ru-RU', 'ru', 'en-US', 'en']
+            });
+            Object.defineProperty(navigator, 'platform', {
+                get: () => 'Win32'
+            });
+            Object.defineProperty(navigator, 'hardwareConcurrency', {
+                get: () => 8
+            });
+            Object.defineProperty(navigator, 'deviceMemory', {
+                get: () => 8
+            });
+            Object.defineProperty(navigator, 'maxTouchPoints', {
+                get: () => 0
+            });
+            window.chrome = {
+                runtime: {}
+            };
+        """)
+        
         return self.page
-
-    async def get_page_content(self, url):
-        if not self.page:
-            await self.start()
-        await self.page.goto(url, wait_until='networkidle')
-        return await self.page.content()
-
-    async def screenshot(self, url):
-        if not self.page:
-            await self.start()
-        await self.page.goto(url, wait_until='networkidle')
-        return await self.page.screenshot(full_page=True)
-
-    async def get_cookies(self, url=None):
-        if not self.page:
-            await self.start()
-        if url:
-            await self.page.goto(url, wait_until='networkidle')
-        return await self.page.context.cookies()
-
-    async def set_cookies(self, cookies_list):
-        if not self.page:
-            await self.start()
-        await self.page.context.add_cookies(cookies_list)
-
-    async def close(self):
-        if self.browser:
-            await self.browser.close()
-            self.browser = None
-            self.page = None
+    
+    # ... остальные методы без изменений
