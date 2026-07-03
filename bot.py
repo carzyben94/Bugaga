d4f554a87ba1f624d571f2c968ef10c95d2ed448
carzyben94 <crazy.ben.laden94@gmail.com>
2026-07-03 12:47:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,25 +3,26 @@ import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# Настройка логирования
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+# Настройка логов
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Токен
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 if not TOKEN:
-    raise ValueError("TELEGRAM_BOT_TOKEN не установлен!")
+    raise ValueError("TELEGRAM_BOT_TOKEN не найден!")
 
 # Команда /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Привет!")
 
 # Запуск
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("🚀 Бот запущен!")
-    app.run_polling(allowed_updates=Update.ALL_TYPES)
+    
+    logger.info("Бот запущен!")
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
