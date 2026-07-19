6aab33cd5e9ffa8d810f823760fe965b8548fd22
carzyben94 <crazy.ben.laden94@gmail.com>
2026-07-19 18:43:40 +0300

    Update cookies.py

--- cookies.py
+++ cookies.py
@@ -1,134 +1,138 @@
-[
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "__cuid",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "55d2d7c5-4888-430a-b024-dd785da46ef4"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "__cuid",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "55d2d7c5-4888-430a-b024-dd785da46ef4"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "lang",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "ru"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "dnt",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "1"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "guest_id",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "v1%3A178267838599411411"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "guest_id_marketing",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "v1%3A178267838599411411"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "guest_id_ads",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "v1%3A178267838599411411"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "personalization_id",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "\"v1_DKrxLZAC902dMFdd1QrVYg==\""
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "twid",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "u%3D2067347503503052800"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "auth_token",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "c9d83e923e1ad6cf67d19a0bc4f9877a49087936"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "ct0",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "39ee0cdf3c0179fb8c50265001cd49e64d652fd3f647e9f091b372641a1d444a1842958c253fe1621a04794de13817dec713e305ed75866c00ecc2a7a0aec112940c06283ca7745b106c4e71a863e3eb"
-  },
-  {
-    "domain": ".x.com",
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "__cf_bm",
-    "path": "\/",
-    "sameSite": "unspecified",
-    "secure": false,
-    "session": true,
-    "value": "rgpecDD.nJZW.PDvUwZ7PnWS.JOBSsUHl1uwuBvlvm0-1784475398.7921503-1.0.1.1-Jh7pm287WlmOXhd1JOwAVbsFYWkIh._GtcIsZAf_n.vP8Os7kJAOjJ.Jg2Rw9cOwixM4iLu0WsOC2uyC6lJfG_cb4Sl1H6fr5jYvSYUr6rNJ.w_I8aFoDCu12CVOpnev"
-  }
+# cookies.py
+
+COOKIES = [
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "__cuid",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "55d2d7c5-4888-430a-b024-dd785da46ef4"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "lang",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "ru"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "dnt",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "1"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "guest_id",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "v1%3A178267838599411411"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "guest_id_marketing",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "v1%3A178267838599411411"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "guest_id_ads",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "v1%3A178267838599411411"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "personalization_id",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": '"v1_DKrxLZAC902dMFdd1QrVYg=="'
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "twid",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "u%3D2067347503503052800"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "auth_token",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "c9d83e923e1ad6cf67d19a0bc4f9877a49087936"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "ct0",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "39ee0cdf3c0179fb8c50265001cd49e64d652fd3f647e9f091b372641a1d444a1842958c253fe1621a04794de13817dec713e305ed75866c00ecc2a7a0aec112940c06283ca7745b106c4e71a863e3eb"
+    },
+    {
+        "domain": ".x.com",
+        "hostOnly": False,
+        "httpOnly": False,
+        "name": "__cf_bm",
+        "path": "/",
+        "sameSite": "unspecified",
+        "secure": False,
+        "session": True,
+        "value": "rgpecDD.nJZW.PDvUwZ7PnWS.JOBSsUHl1uwuBvlvm0-1784475398.7921503-1.0.1.1-Jh7pm287WlmOXhd1JOwAVbsFYWkIh._GtcIsZAf_n.vP8Os7kJAOjJ.Jg2Rw9cOwixM4iLu0WsOC2uyC6lJfG_cb4Sl1H6fr5jYvSYUr6rNJ.w_I8aFoDCu12CVOpnev"
+    }
 ]
+
+# Убираем дубликаты __cuid (оставляем один)
+COOKIES_UNIQUE = []
+seen = set()
+for c in COOKIES:
+    key = (c["name"], c["domain"])
+    if key not in seen:
+        seen.add(key)
+        COOKIES_UNIQUE.append(c)
+
+def get_cookies_for_domain(domain: str) -> list:
+    """Возвращает куки для конкретного домена"""
+    return [c for c in COOKIES_UNIQUE if domain in c.get("domain", "")]
