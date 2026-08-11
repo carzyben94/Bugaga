d98db8e2983b068eee7b310685c39791096892af
carzyben94 <crazy.ben.laden94@gmail.com>
2026-08-11 16:58:16 +0300

    Create xx.py

---
+++ xx.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+import os
+
+CHROME_PATH = "/usr/bin/chromium"
+
+if os.path.exists(CHROME_PATH):
+    subprocess.Popen([
+        CHROME_PATH,
+        "--headless=new",
+        "--no-sandbox",
+        "--remote-debugging-port=9222"
+    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+    time.sleep(3)
+    print("✅ Chrome запущен на порту 9222")
+else:
+    print(f"❌ Chrome не найден по пути {CHROME_PATH}!")
